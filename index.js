76d0f84c0e1ec1a03f7f5d6b9dea6d517f86ea02
David <Davcisneros2000@gmail.com>
2025-08-19 00:33:28 -0400

    Add JS for increment and decrement buttons uisng eventlisteners

--- index.js
+++ index.js
@@ -59,6 +59,26 @@ function addToCart(productBox) {
 // ONCE TRASH BIN ICON IS PRESSED, ITEM REMOVED FROM CART
     cartBox.querySelector(".cart-remove").addEventListener("click", () => {
         cartBox.remove();
-    })
-}
+    });
+
+
+// INCREMENT AND DECREMENT BUTTONS
+    cartBox.querySelector(".cart-quantity").addEventListener("click", event => {
+        const numberElement = cartBox.querySelector(".number");
+        const decrementButton = cartBox.querySelector("#decrement");
+        let quantity = numberElement.textContent;
+
+        if (event.target.id == "decrement" && quantity > 1) {
+            quantity--;
+            if (quantity === 1) {
+                decrementButton.style.color = "#999";
+            }
+        } else if (event.target.id == "increment") {
+            quantity++;
+            decrementButton.style.color = "#333";
+        }
+
+        numberElement.textContent = quantity;
+    });
+};
 
