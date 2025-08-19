79ff3bfe51ddc0e617efba3b8ec3410a1b9cfdbc
David <Davcisneros2000@gmail.com>
2025-08-18 23:38:04 -0400

    Add addToCart function to add product info to shopping cart box

--- index.js
+++ index.js
@@ -4,3 +4,46 @@ const cartClose = document.querySelector("#cart-close");
 
 cartIcon.addEventListener("click", () => cart.classList.add("active"));
 cartClose .addEventListener("click", () => cart.classList.remove("active"));
+
+// EVERY ADD TO CART BUTTON HAS A FUNCTIONALITY TO ADD TO SHOPPING CART
+const addToCartButtons = document.querySelectorAll(".add-cart");
+addToCartButtons.forEach(button => {
+    button.addEventListener("click", event => {
+      const productBox = event.target.closest(".product-box");
+      addToCart(productBox);  
+    });
+});
+
+
+
+// MAKE CART CONTENT BOX 
+const cartContent = document.querySelector(".cart-content");
+
+function addToCart(productBox) {
+    const productImgSrc = productBox.querySelector("img").src;
+    const productTitle = productBox.querySelector(".product-title").textContent;
+    const productPrice = productBox.querySelector(".price").textContent;
+
+    const cartBox = document.createElement("div");
+    cartBox.classList.add("cart-box");
+    cartBox.innerHTML = `
+        <img src="${productImgSrc}" class="cart-img">
+
+                <div class="cart-detail">
+
+                    <h2 class="cart-product-title">${productTitle}</h2>
+                    <span class="cart-price">${productPrice}</span>
+
+                    <div class="cart-quantity">
+                        <button id="decrement">-</button>
+                        <span class="number">0</span>
+                        <button id="increment">+</button>
+                    </div>
+                    
+                </div>
+
+                <i class="fa-solid fa-trash cart-remove"></i>
+    `;
+
+    cartContent.appendChild(cartBox);
+}
