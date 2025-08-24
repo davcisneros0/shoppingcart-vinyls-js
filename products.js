82ea09b03476350426a820f0a3d338ccf359061f
David <Davcisneros2000@gmail.com>
2025-08-24 19:02:29 -0400

    Add two new products

--- products.js
+++ products.js
@@ -27,6 +27,18 @@ const products = [
         description: "Returning to Solid Grooves for his third venture on the label following 2023's solo outing 'Gem' and 2022's 'Uno Dos Tres' with Ben Sterling, he brings a fresh dose of inspiration with his latest single, 'En La Selva'. Teaming up with vocalist Mz Burn, 'En La Selva' is a production that exemplifies his low-slung and heady sound, featuring murky bass tones, crisp rolling drums, and bumping bass hits that work in harmony with the hooky vocals and spiralling synths",
         image: "images/bodeler.webp"
     },
-    // {}, {}, {}, {}
+    {
+        id: 5,
+        title: "Te Quiere",
+        price: "$15",
+        description: "Immerse in the magic of summer with the captivating 'Feeling in Your Soul EP' a collaborative masterpiece by Miguelle, Tons, and the incredibly versatile Miluhska. Prepare to be transported as you dive into the irresistible fusion of gypsy vibes, Spanish guitars, and soulful-jazzy harmonies that define this cultural blend. 'Te Quiere' unfolds from a gaze exchanged between two souls in the heart of the night. With Spanglish lyrics, soulful harmonies, and the vibrant energy of gypsy rhythms, this track takes you on a journey of enjoyment and passion. - House - 130 BPM",
+        image: "images/feelinginyoursoul.jpg"
+    }, {
+        id: 6,
+        title: "Searching",
+        price: "$20",
+        description: "Two of the UKs favourite house talents, East End Dubs and Wheats have been pushing their distinctive takes on the genre to new heights of late following another non-stop summer, which included notable returns to SGs Casino Motel at DC10 for both.  - House - 130 BPM",
+        image: "images/eastenddubs.jpg"
+    },
 
 ]
